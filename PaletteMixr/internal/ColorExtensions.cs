2e8d240b9b9024724e2e19de2e35605b205f9434
Ben Crevis <iam@bencrevis.uk>
2019-03-02 17:11:42 +0000

    Refactor HSL conversion

--- PaletteMixr/internal/ColorExtensions.cs
+++ PaletteMixr/internal/ColorExtensions.cs
@@ -7,48 +7,82 @@ namespace PaletteMixr
     {
         internal static HslColor ToHsl(this Color color)
         {
-            double r = color.R / 255d;
-            double g = color.G / 255d;
-            double b = color.B / 255d;
+            (double r, double g, double b) = Normalise(color);
 
-            double max = Math.Max(r, Math.Max(g, b));
-            double min = Math.Min(r, Math.Min(g, b));
+            (double min, double max) = GetMinAndMax(r, g, b);
 
-            double h, s, l = (max + min) / 2d;
+            double h, s, l;
 
-            if (max == min)
+            l = CalculateLuminosity(min, max);
+
+            if (ShadeOfGrey(min, max))
             {
                 h = s = 0;
             }
             else
             {
-                var d = max - min;
-                if (l > 0.5d)
-                {
-                    s = d / (2d - max - min);
-                }
-                else
-                {
-                    s = d / (max + min);
-                }
+                double minMaxDelta = max - min;
 
-                if (max == r)
-                {
-                    h = ((g - b) / d) + (g < b ? 6d : 0);
-                }
-                else if(max == g)
-                {
-                    h = ((b - r) / d) + 2d;
-                }
-                else
-                {
-                    h = ((r - g) / d) + 4d;
-                }
-
-                h /= 6;
+                s = CalculateSaturation(min, max, l, minMaxDelta);
+                h = CalculateHue(r, g, b, max, minMaxDelta);
             }
 
             return new HslColor(h, s, l);
+        }
+
+        private static (double r, double g, double b) Normalise(Color color)
+        {
+            return (color.R / 255d, color.G / 255d, color.B / 255d);
+        }
+
+        private static (double min, double max) GetMinAndMax(double r, double g, double b)
+        {
+            double min = Math.Min(r, Math.Min(g, b));
+            double max = Math.Max(r, Math.Max(g, b));
+
+            return (min, max);
+        }
+
+        private static bool ShadeOfGrey(double min, double max)
+        {
+            return max == min;
+        }
+
+        private static double CalculateLuminosity(double min, double max)
+        {
+            return (max + min) / 2d;
+        }
+
+        private static double CalculateSaturation(double min, double max, double l, double minMaxDelta)
+        {
+            if (l > 0.5d)
+            {
+                return minMaxDelta / (2d - minMaxDelta);
+            }
+            else
+            {
+                return minMaxDelta / (max + min);
+            }
+        }
+
+        private static double CalculateHue(double r, double g, double b, double max, double minMaxDelta)
+        {
+            double h;
+            if (max == r)
+            {
+                double v = g < b ? 6d : 0;
+                h = ((g - b) / minMaxDelta) + v;
+            }
+            else if (max == g)
+            {
+                h = ((b - r) / minMaxDelta) + 2d;
+            }
+            else
+            {
+                h = ((r - g) / minMaxDelta) + 4d;
+            }
+
+            return h /= 6;
         }
     }
 }
